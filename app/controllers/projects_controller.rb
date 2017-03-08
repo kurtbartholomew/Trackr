f9026de5a2ef32b490f1679df5deee80964c5e95
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-07 18:56:24 -0800

    (feat) Changed new project and delete project functionality to admin-only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,21 +5,7 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash.now[:alert] = "Project has not been created."
-      render "new"
-    end
-  end
+  
 
   def edit
   end
@@ -37,12 +23,7 @@ class ProjectsController < ApplicationController
   def show
   end
 
-  def destroy
-    @project.destroy
-
-    flash[:notice] = "Project has been deleted."
-    redirect_to projects_path
-  end
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
+
+    resources :projects, only: [:new, :create, :destroy]
   end
 
   devise_for :users
@@ -59,7 +61,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root "projects#index"
-  resources :projects do
+  resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
 end
