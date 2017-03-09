007033e0b4d298a236c81ade5ad9cf7590d54f9e
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-08 21:29:12 -0800

    (feat) Restricted editing projects to only admins and managers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,12 +5,14 @@ class ProjectsController < ApplicationController
     @projects = policy_scope(Project)
   end
 
-  
+    
 
   def edit
+    authorize @project, :update?
   end
 
   def update
+    authorize @project, :update?
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
