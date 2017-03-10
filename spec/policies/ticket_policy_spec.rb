2eb32e92a9ae6afd54795fd1edd88d0a95093c69
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-09 21:29:25 -0800

    (feat) Added permissions for editing tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,57 +1,68 @@
 require 'rails_helper'
 
-# RSpec.describe TicketPolicy do
-#   context "permissions" do
-#     let(:user) { FactoryGirl.create(:user) }
-#     let(:project) { FactoryGirl.create(:project) }
-#     let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+RSpec.describe TicketPolicy do
+  context "permissions" do
+    subject { TicketPolicy.new(user, ticket) }
 
-#     subject { ProjectPolicy.new(user, ticket) }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
+    let(:ticket) { FactoryGirl.create(:ticket, project: project) }
 
-#     context "for anonymous users" do
-#       let(:user) { nil }
-#       byebug
-#       it { should_not permit_action :show }
-#       it { should_not permit_action :create }
-#     end
+    context "for anonymous users" do
+      let(:user) { nil }
+      it { should_not permit_action :show }
+      it { should_not permit_action :create }
+      it { should_not permit_action :update }
+    end
 
-#     context "for views of the project" do
-#       before { assign_role!(user, :viewer, project) }
+    context "for views of the project" do
+      before { assign_role!(user, :viewer, project) }
 
-#       it { should permit_action :show }
-#       it { should_not permit_action :create }
-#     end
+      it { should permit_action :show }
+      it { should_not permit_action :create }
+      it { should_not permit_action :update }
+    end
 
-#     context "for editors of the project" do
-#       before { assign_role!(user, :editor, project) }
+    context "for editors of the project" do
+      before { assign_role!(user, :editor, project) }
 
-#       it { should permit_action :show }
-#       it { should permit_action :create }
-#     end
+      it { should permit_action :show }
+      it { should permit_action :create }
+      it { should_not permit_action :update }
 
-#     context "for managers of the project" do
-#       before { assign_role!(user, :manager, project)}
+      context "when the editor created the ticket" do
+        before { ticket.author = user }
 
-#       it { should permit_action :show }
-#       it { should permit_action :create }
-#     end
+        it { should permit_action :update }
+      end
+    end
 
-#     context "for managers of other projects" do
-#       before do
-#         assign_role!(user, :manager, FactoryGirl.create(:project))
-#       end
+    context "for managers of the project" do
+      before { assign_role!(user, :manager, project)}
 
-#       it { should_not permit_action :show }
-#       it { should_not permit_action :create }
-#     end
+      it { should permit_action :show }
+      it { should permit_action :create }
+      it { should permit_action :update }
+    end
 
-#     context "for administrators" do
-#       let(:user) { FactoryGirl.create :user, :admin }
+    context "for managers of other projects" do
+      before do
+        assign_role!(user, :manager, FactoryGirl.create(:project))
+      end
 
-#       it { should permit_action :show }
-#       it { should permit_action :create }
-#     end
-#   end
-# end
+      it { should_not permit_action :show }
+      it { should_not permit_action :create }
+      it { should_not permit_action :update }
+    end
+
+    context "for administrators" do
+      let(:user) { FactoryGirl.create :user, :admin }
+
+      it { should permit_action :show }
+      it { should permit_action :create }
+      it { should permit_action :update }
+    end
+  end
+end
 
 # DOESN"T WORK FOR NO GODDAMN REASON
