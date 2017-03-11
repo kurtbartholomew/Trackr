d8d4d146612c9b88dc43d0664e4deffd8885068d
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-10 20:47:48 -0800

    (feat) Allow admins to assign roles when creating new users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,6 +7,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def create
     @user = User.new(user_params)
+    build_roles_for(@user)
 
     if @user.save
       flash[:notice] = "User has been created."
@@ -34,6 +35,7 @@ class Admin::UsersController < Admin::ApplicationController
 
     User.transaction do
       @user.roles.clear
+      build_roles_for(@user)
       role_data = params.fetch(:roles, [])
       role_data.each do |project_id, role_name|
         if role_name.present?
@@ -74,5 +76,13 @@ class Admin::UsersController < Admin::ApplicationController
 
     def user_params
       params.require(:user).permit(:email, :password, :admin)
+    end
+    def build_roles_for(user)
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+       if role_name.present?
+         user.roles.build(project_id: project_id, role: role_name) 
+       end 
+      end
     end
 end
