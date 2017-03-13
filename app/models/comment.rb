a0839be99ed09cc9da08c3ac352942181e0d8cc4
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-12 20:18:40 -0700

    (feat) Added recording and display of state transitions in ticket comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,11 @@
 class Comment < ActiveRecord::Base
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   belongs_to :state
   belongs_to :ticket
   belongs_to :author, class_name: "User"
+  belongs_to :previous_state, class_name: "State"
   scope :persisted, lambda { where.not(id: nil) }
 
   validates :text, presence: true
@@ -14,4 +16,7 @@ class Comment < ActiveRecord::Base
       ticket.state = state
       ticket.save!
     end  
+    def set_previous_state
+      self.previous_state = ticket.state
+    end
 end
