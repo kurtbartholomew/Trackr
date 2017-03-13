520e8f5c3600b145e4df4fffa58bde96c84932cc
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-03-12 19:27:21 -0700

    (feat) Added ability to set state on tickets

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,17 @@
 class Comment < ActiveRecord::Base
+  after_create :set_ticket_state
+
+  belongs_to :state
   belongs_to :ticket
   belongs_to :author, class_name: "User"
   scope :persisted, lambda { where.not(id: nil) }
 
   validates :text, presence: true
   delegate :project, to: :ticket
+
+  private
+    def set_ticket_state
+      ticket.state = state
+      ticket.save!
+    end  
 end
